5e513c2352e5dd74abd7de088cdecd22638ac113
Luis Roberto Oliveira Santos <TNEP00114@equatorial.corp>
2021-06-16 14:53:00 -0300

    feat: create and list categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,28 @@
 import { Router } from 'express';
 
+import CategoriesRepository from '../repositories/CategoriesRepository';
+
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories = [];
-
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({ name, description });
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category already exists' });
+  }
+
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const categories = categoriesRepository.list();
+
+  return response.json(categories);
 });
 
 export default categoriesRoutes;
