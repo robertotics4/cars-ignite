5963383db61d6a71e071f78f3965afb7e0708920
robertotics4 <robertooliveiragp@gmail.com>
2021-09-20 14:28:27 -0300

    fix: category repository bug fixed

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -4,7 +4,9 @@ import ListCategoriesUseCase from './ListCategoriesUseCase';
 
 export default (): ListCategoriesController => {
   const categoriesRepository = new CategoriesRepository();
+
   const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
+
   const listCategoriesController = new ListCategoriesController(
     listCategoriesUseCase,
   );
