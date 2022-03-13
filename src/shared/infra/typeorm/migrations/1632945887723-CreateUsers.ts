f14aea23832911239ca97f339830580458cffdd0
robertotics4 <robertooliveiragp@gmail.com>
2022-03-13 15:25:17 -0300

    feat: create rental migration

--- src/shared/infra/typeorm/migrations/1632945887723-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1632945887723-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1632945887723 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
